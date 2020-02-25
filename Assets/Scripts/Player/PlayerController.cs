08108ea6cb539a1c548049246e17ed714f47aac4
eamonboyle <blaowskate@hotmail.com>
2020-02-25 19:40:10 +0000

    Add touch controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,152 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public enum Lane
+    {
+        Left,
+        Center,
+        Right
+    }
+
+    public enum MoveDirection
+    {
+        Left,
+        Right,
+        None
+    }
+
+    public float speed = 5.0f;
+
+    public bool jumping = false;
+    public bool sliding = false;
+
+    public Animator anim;
+
+    private Lane currentLane = Lane.Center;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, 0.1f);
+        // move forwards
+        transform.Translate(0, 0, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumping = true;
+        }
+        else
+        {
+            jumping = false;
+        }
+
+        if (jumping)
+        {
+            anim.SetBool("isJumping", true);
+
+            transform.Translate(0, 0.1f, 0.1f);
+        }
+        else
+        {
+            anim.SetBool("isJumping", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            sliding = true;
+        }
+        else
+        {
+            sliding = false;
+        }
+
+        if (sliding)
+        {
+            anim.SetBool("isSliding", true);
+        }
+        else
+        {
+            anim.SetBool("isSliding", false);
+        }
+
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    transform.Translate(-4.1f, 0, 0);
+        //}
+
+
+        // HANDLE LANE MOVEMENT
+        MoveDirection requestedMoveDirection = MoveDirection.None;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            requestedMoveDirection = MoveDirection.Left;
+        } 
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            requestedMoveDirection = MoveDirection.Right;
+        }
+
+        HandleLaneMovement(requestedMoveDirection);
+    }
+
+    private void HandleLaneMovement(MoveDirection requestedMoveDirection)
+    {
+        switch (requestedMoveDirection)
+        {
+            case MoveDirection.Left:
+                if (currentLane == Lane.Left)
+                {
+                    break;
+                }
+                else if (currentLane == Lane.Center)
+                {
+                    MoveLeft();
+                    currentLane = Lane.Left;
+                }
+                else if (currentLane == Lane.Right)
+                {
+                    MoveLeft();
+                    currentLane = Lane.Center;
+                }
+                break;
+            case MoveDirection.Right:
+                if (currentLane == Lane.Right)
+                {
+                    break;
+                }
+                else if (currentLane == Lane.Center)
+                {
+                    MoveRight();
+                    currentLane = Lane.Right;
+                }
+                else if (currentLane == Lane.Left)
+                {
+                    MoveRight();
+                    currentLane = Lane.Center;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void MoveRight()
+    {
+        Debug.Log("Move Right");
+    }
+
+    private void MoveLeft()
+    {
+        Debug.Log("Move Left");
     }
 }
---
+++ Assets/Scripts/Player/PlayerControllerTouch.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerTouch : MonoBehaviour
+{
+    public enum Lane
+    {
+        Left,
+        Center,
+        Right
+    }
+
+    public enum MoveDirection
+    {
+        Left,
+        Right,
+        None
+    }
+
+    public const float MAX_SWIPE_TIME = 0.5f;
+
+    public const float MIN_SWIPE_DISTANCE = 0.17f;
+
+    public static bool swipedRight = false;
+    public static bool swipedLeft = false;
+    public static bool swipedUp = false;
+    public static bool swipedDown = false;
+
+    public bool debugWithArrowKeys = true;
+
+    private Vector2 startPos;
+    private float startTime;
+    private Lane currentLane = Lane.Center;
+
+    // Update is called once per frame
+    void Update()
+    {
+        swipedRight = false;
+        swipedLeft = false;
+        swipedUp = false;
+        swipedDown = false;
+        MoveDirection requestedMoveDirection = MoveDirection.None;
+        HandleLaneMovement(requestedMoveDirection);
+
+        if (Input.touches.Length > 0)
+        {
+            Touch t = Input.GetTouch(0);
+
+            if (t.phase == TouchPhase.Began)
+            {
+                startPos = new Vector2(t.position.x / (float)Screen.width, t.position.y / (float)Screen.width);
+                startTime = Time.time;
+            }
+
+            if (t.phase == TouchPhase.Ended)
+            {
+                // pressed too long
+                if (Time.time - startTime > MAX_SWIPE_TIME)
+                {
+                    return;
+                }
+
+                Vector2 endPos = new Vector2(t.position.x / (float)Screen.width, t.position.y / (float)Screen.width);
+                Vector2 swipe = new Vector2(endPos.x - startPos.x, endPos.y - startPos.y);
+
+                // too short swipe
+                if (swipe.magnitude < MIN_SWIPE_DISTANCE)
+                {
+                    return;
+                }
+
+                if (Mathf.Abs(swipe.x) > Mathf.Abs(swipe.y))
+                {
+                    if (swipe.x > 0)
+                    {
+                        swipedRight = true;
+                        requestedMoveDirection = MoveDirection.Right;
+                        HandleLaneMovement(requestedMoveDirection);
+                    }
+                    else
+                    {
+                        swipedLeft = true;
+                        requestedMoveDirection = MoveDirection.Left;
+                        HandleLaneMovement(requestedMoveDirection);
+                    }
+                }
+                else
+                {
+                    if (swipe.y > 0)
+                    {
+                        swipedUp = true;
+                    }
+                    else
+                    {
+                        swipedDown = true;
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void HandleLaneMovement(MoveDirection requestedMoveDirection)
+    {
+        Debug.Log(currentLane);
+        switch (requestedMoveDirection)
+        {
+            case MoveDirection.Left:
+                if (currentLane == Lane.Left)
+                {
+                    break;
+                }
+                else if (currentLane == Lane.Center)
+                {
+                    MoveLeft();
+                    currentLane = Lane.Left;
+                }
+                else if (currentLane == Lane.Right)
+                {
+                    MoveLeft();
+                    currentLane = Lane.Center;
+                }
+                break;
+            case MoveDirection.Right:
+                if (currentLane == Lane.Right)
+                {
+                    break;
+                }
+                else if (currentLane == Lane.Center)
+                {
+                    MoveRight();
+                    currentLane = Lane.Right;
+                }
+                else if (currentLane == Lane.Left)
+                {
+                    MoveRight();
+                    currentLane = Lane.Center;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void MoveRight()
+    {
+        Debug.Log("Move Right");
+        Debug.Log(currentLane);
+    }
+
+    private void MoveLeft()
+    {
+        Debug.Log("Move left");
+    }
+}
