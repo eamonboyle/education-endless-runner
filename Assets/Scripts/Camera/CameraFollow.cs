d35d38fae4090de5072ca1a0d463d73360778392
eamonboyle <blaowskate@hotmail.com>
2020-02-26 18:50:42 +0000

    Add 3d text and changed the camera script to only follow on the z axis

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -7,15 +7,18 @@ public class CameraFollow : MonoBehaviour
     public GameObject player;
 
     private Vector3 offset;
+    private float offset2;
 
     // Start is called before the first frame update
     void Start()
     {
         offset = transform.position - player.transform.position;
+        offset2 = transform.position.z - player.transform.position.z;
     }
 
     private void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        //transform.position = player.transform.position + offset;
+        transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z + offset2);
     }
 }
