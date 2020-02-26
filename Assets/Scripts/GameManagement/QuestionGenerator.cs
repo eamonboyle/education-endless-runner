3dddc9c552817a9467be361497ae6ed8761a63e0
eamonboyle <blaowskate@hotmail.com>
2020-02-26 20:27:23 +0000

    Question boxes now disappear when the user collides, makes for a better gameplay perspective

--- Assets/Scripts/GameManagement/QuestionGenerator.cs
+++ Assets/Scripts/GameManagement/QuestionGenerator.cs
@@ -14,10 +14,14 @@ public class QuestionGenerator : MonoBehaviour
         Division
     }
 
+    public GameObject player;
     public GameObject questionBox;
     public GameObject questionText;
 
-    private bool questionExists = false;
+    public List<GameObject> questionBoxes;
+
+    public bool questionExists = false;
+
     private QuestionType questionType;
     private int number1;
     private int number2;
@@ -36,34 +40,51 @@ public class QuestionGenerator : MonoBehaviour
         {
             Debug.Log("GENERATE A QUESTION");
 
-            number1 = Random.Range(0, 200);
-            number2 = Random.Range(0, 200);
-            answer = number1 + number2;
-
-            Debug.Log(number1 + " + " + number2 + " = " + answer);
-
-            questionText.GetComponent<Text>().text = number1.ToString() + " + " + number2.ToString();
-
-            // right box
-            GameObject rightBox = Instantiate(questionBox, new Vector3(1.586f, 1.3f, 19.0f), Quaternion.identity, null);
-            rightBox.GetComponent<QuestionBox>().number = answer;
-            rightBox.GetComponentInChildren<TextMeshPro>().text = answer.ToString();
-
-            // center
-            int wrong1 = Random.Range(0, 200);
-            GameObject centerBox = Instantiate(questionBox, new Vector3(0f, 1.3f, 19.0f), Quaternion.identity, null);
-            centerBox.GetComponent<QuestionBox>().number = wrong1;
-            centerBox.GetComponentInChildren<TextMeshPro>().text = wrong1.ToString();
-
-            // left
-            int wrong2 = Random.Range(0, 200);
-            GameObject leftBox = Instantiate(questionBox, new Vector3(-1.586f, 1.3f, 19.0f), Quaternion.identity, null);
-            leftBox.GetComponent<QuestionBox>().number = wrong2;
-            leftBox.GetComponentInChildren<TextMeshPro>().text = wrong2.ToString();
-
-
+            GenerateQuestion();
 
             questionExists = true;
         }
+    }
+
+    public void GenerateQuestion()
+    {
+        number1 = Random.Range(0, 200);
+        number2 = Random.Range(0, 200);
+        answer = number1 + number2;
+
+        Debug.Log(number1 + " + " + number2 + " = " + answer);
+
+        questionText.GetComponent<Text>().text = number1.ToString() + " + " + number2.ToString();
+
+        // work out the zindex to spawn the boxes at
+        float spawnZ = Random.Range(player.transform.position.z + 15.0f, player.transform.position.z + 25.0f);
+        //float spawnZ = player.transform.position.z + 20.0f;
+
+
+        // MAKE THE NUMBER SPAWN IN RANDOM LANES
+
+        // right box
+        GameObject rightBox = Instantiate(questionBox, new Vector3(1.586f, 1.3f, spawnZ), Quaternion.identity, null);
+        rightBox.GetComponent<QuestionBox>().number = answer;
+        rightBox.GetComponent<QuestionBox>().correctNumber = answer;
+        rightBox.GetComponentInChildren<TextMeshPro>().text = answer.ToString();
+
+        // center
+        int wrong1 = Random.Range(0, 200);
+        GameObject centerBox = Instantiate(questionBox, new Vector3(0f, 1.3f, spawnZ), Quaternion.identity, null);
+        centerBox.GetComponent<QuestionBox>().number = wrong1;
+        centerBox.GetComponent<QuestionBox>().correctNumber = answer;
+        centerBox.GetComponentInChildren<TextMeshPro>().text = wrong1.ToString();
+
+        // left
+        int wrong2 = Random.Range(0, 200);
+        GameObject leftBox = Instantiate(questionBox, new Vector3(-1.586f, 1.3f, spawnZ), Quaternion.identity, null);
+        leftBox.GetComponent<QuestionBox>().number = wrong2;
+        leftBox.GetComponent<QuestionBox>().correctNumber = answer;
+        leftBox.GetComponentInChildren<TextMeshPro>().text = wrong2.ToString();
+
+        questionBoxes.Add(rightBox);
+        questionBoxes.Add(leftBox);
+        questionBoxes.Add(centerBox);
     }
 }
--- Assets/Scripts/Questions/QuestionBox.cs
+++ Assets/Scripts/Questions/QuestionBox.cs
@@ -1,19 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class QuestionBox : MonoBehaviour
 {
     public int number;
+    public int correctNumber;
+
+    private new GameObject camera;
 
     // Start is called before the first frame update
     void Start()
     {
+        camera = GameObject.FindWithTag("MainCamera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // delete the boxes once they go off camera
+        //if ((transform.position.z - 5.0f) < camera.transform.position.z)
+        //{
+        //    Destroy(gameObject);
+        //}
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            return;
+        }
+
+        Debug.Log("COLLIDED WITH: " + other.gameObject.name);
+
+        // work out if right or wrong
+        if (number != correctNumber)
+        {
+            // wrong
+            SceneManager.LoadScene(3);
+            return;
+        }
+
+        // find the game manager gameobject
+        QuestionGenerator questionGenerator = GameObject.FindWithTag("GameManager").GetComponent<QuestionGenerator>();
+
+        // delete these question boxes
+        Destroy(questionGenerator.questionBoxes[0].gameObject);
+        Destroy(questionGenerator.questionBoxes[1].gameObject);
+        Destroy(questionGenerator.questionBoxes[2].gameObject);
+        questionGenerator.questionBoxes.RemoveRange(0, 3);
+
+        // set the bool question to false
+        // to generate a new question
+        questionGenerator.questionExists = false;
     }
 }
