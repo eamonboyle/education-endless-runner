d48c7eeb12c8e1734ec93f555c625ed3a06be8ea
eamonboyle <blaowskate@hotmail.com>
2020-02-26 23:07:35 +0000

    Add a quick scoring mechanic

---
+++ Assets/Scripts/GameManagement/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public GameObject scoreObject;
+    private float timePassed = 0.0f;
+    private int score = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreObject.GetComponent<TextMeshProUGUI>().text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        if (timePassed >= 1)
+        {
+            score += (int)timePassed;
+            timePassed -= (int)timePassed;
+        }
+
+        scoreObject.GetComponent<TextMeshProUGUI>().text = "Score: " + score;
+        PlayerPrefs.SetInt("score", score);
+    }
+}
--- Assets/Scripts/Menu/GameOver.cs
+++ Assets/Scripts/Menu/GameOver.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    public GameObject scoreObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int score = PlayerPrefs.GetInt("score");
+        scoreObject.GetComponent<TextMeshProUGUI>().text = "You scored: " + score;
     }
 
     // Update is called once per frame
