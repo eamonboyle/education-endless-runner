3a30cf3c754e189edb963da373227e2f1bcddef6
eamonboyle <blaowskate@hotmail.com>
2020-02-27 20:08:53 +0000

    Add fonts and new menu

--- Assets/Scripts/GameManagement/QuestionGenerator.cs
+++ Assets/Scripts/GameManagement/QuestionGenerator.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 namespace Assets.Scripts.GameManagement
 {
@@ -30,6 +31,8 @@ namespace Assets.Scripts.GameManagement
         private QuestionType questionType;
         private int number1;
         private int number2;
+        private int wrongNumber1;
+        private int wrongNumber2;
         private int answer;
 
         // Start is called before the first frame update
@@ -58,33 +61,21 @@ namespace Assets.Scripts.GameManagement
 
         public void GenerateQuestion()
         {
-            // TODO: increase these ranges as time passes
-            number1 = Random.Range(0, 70);
-            number2 = Random.Range(0, 70);
-
-            if (number1 == number2)
-            {
-                number2 = Random.Range(0, 30);
-            }
-
-            answer = number1 + number2;
-
-            // debug
-            Debug.Log(number1 + " + " + number2 + " = " + answer);
+            CalculateNumbers();
 
             questionText.GetComponent<Text>().text = number1.ToString() + " + " + number2.ToString();
 
             // work out the zindex to spawn the boxes at
             // make this shorter when the difficulty increases
-            float spawnZ = Random.Range(player.transform.position.z + 15.0f, player.transform.position.z + 25.0f);
+            float spawnZ = UnityEngine.Random.Range(player.transform.position.z + 15.0f, player.transform.position.z + 25.0f);
 
 
             // MAKE THE NUMBER SPAWN IN RANDOM LANES
 
             // put the numbers into a list
             List<int> numberList = new List<int>();
-            numberList.Add(number1);
-            numberList.Add(number2);
+            numberList.Add(wrongNumber1);
+            numberList.Add(wrongNumber2);
             numberList.Add(answer);
 
             // test randomMizer
@@ -105,14 +96,14 @@ namespace Assets.Scripts.GameManagement
             rightBox.GetComponentInChildren<TextMeshPro>().text = rightBox.GetComponent<QuestionBox>().number.ToString();
 
             // center
-            int wrong1 = Random.Range(0, 200);
+            int wrong1 = UnityEngine.Random.Range(0, 200);
             GameObject centerBox = Instantiate(questionBox, new Vector3(0f, 1.3f, spawnZ), Quaternion.identity, null);
             centerBox.GetComponent<QuestionBox>().number = numberList[1];
             centerBox.GetComponent<QuestionBox>().correctNumber = answer;
             centerBox.GetComponentInChildren<TextMeshPro>().text = centerBox.GetComponent<QuestionBox>().number.ToString();
 
             // left
-            int wrong2 = Random.Range(0, 200);
+            int wrong2 = UnityEngine.Random.Range(0, 200);
             GameObject leftBox = Instantiate(questionBox, new Vector3(-1.586f, 1.3f, spawnZ), Quaternion.identity, null);
             leftBox.GetComponent<QuestionBox>().number = numberList[2];
             leftBox.GetComponent<QuestionBox>().correctNumber = answer;
@@ -121,6 +112,34 @@ namespace Assets.Scripts.GameManagement
             questionBoxes.Add(rightBox);
             questionBoxes.Add(leftBox);
             questionBoxes.Add(centerBox);
+        }
+
+        private void CalculateNumbers()
+        {
+            // TODO: increase these ranges as time passes
+            number1 = UnityEngine.Random.Range(0, 70);
+            number2 = UnityEngine.Random.Range(0, 70);
+
+            if (number1 == number2)
+            {
+                number2 = UnityEngine.Random.Range(0, 70);
+            }
+
+            answer = number1 + number2;
+
+            // as time goes on calculate this on a % range from the answer
+            //wrongNumber1 = UnityEngine.Random.Range(0, 70);
+            //wrongNumber2 = UnityEngine.Random.Range(0, 70);
+            wrongNumber1 = UnityEngine.Random.Range(0, 70);
+            wrongNumber2 = UnityEngine.Random.Range(0, 70);
+
+            if (wrongNumber1 == wrongNumber2)
+            {
+                wrongNumber1 = UnityEngine.Random.Range(0, 70);
+            }
+
+            // debug
+            Debug.Log(number1 + " + " + number2 + " = " + answer);
         }
 
         private List<int> RandomizeBoxPlacement(List<int> numbers)
