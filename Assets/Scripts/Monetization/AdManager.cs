4b749133470e4826843090f3f308cf6a42f594b0
eamonboyle <blaowskate@hotmail.com>
2020-03-02 19:06:17 +0000

    Got the start of ads working

---
+++ Assets/Scripts/Monetization/AdManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour, IUnityAdsListener
+{
+    private string gameId = "3492332";
+    private bool testMode = true;
+    private string placementIdReward = "rewardedVideo";
+    private string placementIdVideo = "video";
+
+    public void OnUnityAdsDidError(string message)
+    {
+        Debug.Log("AD ERROR");
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        // Define conditional logic for each ad completion status:
+        if (showResult == ShowResult.Finished)
+        {
+            // Reward the user for watching the ad to completion.
+            Debug.Log("FINISHED AD");
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+            // Do not reward the user for skipping the ad.
+            Debug.Log("SKIPPED ADD");
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+            Debug.LogWarning("The ad did not finish due to an error.");
+        }
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        Debug.Log("STARTED");
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        Debug.Log("AD READY");
+        if (placementId == placementIdVideo)
+        {
+            Advertisement.Show(placementIdVideo);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            gameId = "3492333";
+        }
+
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(gameId, testMode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
