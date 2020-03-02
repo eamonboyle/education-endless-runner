a1d85ce56a1516200873a0ce17c4517a2335ef07
eamonboyle <blaowskate@hotmail.com>
2020-03-02 18:26:07 +0000

    Removed the unity ads from asset store

--- Assets/Scripts/Menu/GameOver.cs
+++ Assets/Scripts/Menu/GameOver.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//using UnityEngine.Advertisements;
 
 public class GameOver : MonoBehaviour
 {
     public GameObject scoreObject;
+
+    //private string gameId = "3492332";
+    //private bool testMode = true;
 
     // Start is called before the first frame update
     void Start()
     {
         int score = PlayerPrefs.GetInt("score");
         scoreObject.GetComponent<Text>().text = "Score: " + score;
+
+        //Advertisement.Initialize(gameId, testMode);
+
+        //Advertisement.Show();
     }
 
     public void RestartGame()
