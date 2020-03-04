adb802ef96d85f012415efbe2c498241a40aeca5
eamonboyle <blaowskate@hotmail.com>
2020-03-04 22:01:36 +0000

    Create new character select menu

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+
+        animator.SetBool("dancing", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
