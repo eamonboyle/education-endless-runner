6d4f8f92b60950573928d25195a680886344b9af
eamonboyle <blaowskate@hotmail.com>
2020-03-05 18:41:50 +0000

    Fixed the floor disappearing bug

--- Assets/Scripts/GameManagement/LevelGenerator.cs
+++ Assets/Scripts/GameManagement/LevelGenerator.cs
@@ -11,6 +11,8 @@ public class LevelGenerator : MonoBehaviour
     public float floorWidth = 36.0f;
     public float currentPlace = 36.0f;
 
+    int floorCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,20 +31,38 @@ public class LevelGenerator : MonoBehaviour
     {
         if (player.transform.position.z > (currentPlace + 4.0f))
         {
+            Debug.Log("GENERATE FLOOR");
+
+            floorCount++;
+
             int floorDegreeRandom = Random.Range(0, 2);
 
             Vector3 floorPlacement = new Vector3(0.0f, 0.0f, currentPlace + (floorWidth * 2) + 18.44f);
-            Quaternion floorRotation = Quaternion.Euler(0.0f, floorDegreeRandom == 1 ? 180.0f : 0.0f, 0.0f);
+            //Quaternion floorRotation = Quaternion.Euler(0.0f, floorDegreeRandom == 1 ? 180.0f : 0.0f, 0.0f);
+            Quaternion floorRotation = Quaternion.identity;
 
             GameObject nextFloor = Instantiate(floorPrefab, floorPlacement, floorRotation, levelContainer.transform);
 
             floorPieces.Add(nextFloor);
 
-            // delete last one
-            Destroy(floorPieces[0]);
-            floorPieces.RemoveAt(0);
+            if (floorCount == 2)
+            {
+                Destroy(floorPieces[0]);
+                floorPieces.RemoveAt(0);
+
+                floorCount = 0;
+            }
 
             currentPlace += floorWidth;
         }
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        Debug.Log("REMOVE FLOOR");
+        yield return new WaitForSeconds(time);
+
+        Destroy(floorPieces[0]);
+        floorPieces.RemoveAt(0);
     }
 }
