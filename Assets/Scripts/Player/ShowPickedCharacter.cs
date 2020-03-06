05d575a729cac7c88152f3921ec37b9e7b244569
eamonboyle <blaowskate@hotmail.com>
2020-03-06 23:31:36 +0000

    Changed the menu to show the character and over the map

---
+++ Assets/Scripts/Player/ShowPickedCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPickedCharacter : MonoBehaviour
+{
+    public GameObject boy;
+    public GameObject girl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string player = PlayerPrefs.GetString("character");
+
+        if (player == "girl")
+        {
+            girl.SetActive(true);
+            boy.SetActive(false);
+        }
+        else
+        {
+            girl.SetActive(false);
+            boy.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
